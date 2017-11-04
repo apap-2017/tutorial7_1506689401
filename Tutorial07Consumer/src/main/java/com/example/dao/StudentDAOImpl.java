f20d7ea532ceab5a36a3093643244c3ef8d35169
fajarbardan <fajarbardan@gmail.com>
2017-11-04 21:59:07 +0700

    push final tutorial 7, fixed bugs

--- Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
+++ Tutorial07Consumer/src/main/java/com/example/dao/StudentDAOImpl.java
@@ -3,14 +3,22 @@ package com.example.dao;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.dao.StudentDAO;
 import com.example.model.StudentModel;
 
+@Service
 public class StudentDAOImpl implements StudentDAO{
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	
 	@Override
 	public StudentModel selectStudent(String npm) {
